7da1fe1cb794b0cbbbf6ff30d3d55169ea9b219c
anushapanchadsharam <panchadsharamanusha@gmail.com>
2023-07-12 21:52:10 +0530

    Final Modification

--- src/components/suggestions.js
+++ src/components/suggestions.js
@@ -1,10 +1,13 @@
 
+
 import { useState } from "react";
 import './suggestions.css'
-function Car(){
-    const [suggest, setSuggest] = useState("");
 
-  let handleSubmit = async (e) => {
+function Suggestion() {
+  const [suggest, setSuggest] = useState("");
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       let res = await fetch("http://localhost:5000/api/suggestions/add", {
@@ -13,51 +16,70 @@ function Car(){
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            suggest: suggest,
-         
+          suggest: suggest,
         }),
       });
       let resJson = await res.json();
       console.log(resJson);
       if (res.status === 200) {
-        alert("suggestions added successfully");
+        alert("Suggestions added successfully");
       } else {
-        alert("unknown error on uploading");
+        alert("Unknown error on uploading");
       }
     } catch (err) {
       console.log(err);
     }
   };
-return(
-  <div className="container">
-      <div className="row" >
-       
-              <div className="col-md-3"></div>
-       
-              <div className="col-md-6"  >
-          
-                             <div className="comment" >Your Comments</div>  
-               </div>
-              <div className="col-md-3"></div> 
-      </div>
-        
-       <div className="row">
-        <div className="col-8 mx-auto m-5">
-             <form onSubmit={handleSubmit}>
-         <div className="form-group " style={{height:"500px"}}onSubmit={handleSubmit} >
-         <label for="exampleFormControlTextarea1" id="label1">we are eager to get your commands</label>
-    {/* <textarea className="form-control" id="exampleFormControlTextarea1"  style={{height:"500px"}} onChange={(e) => setSuggest(e.target.value)}>
-    <label for="exampleFormControlTextarea1" ></label>
-    </textarea>  */}
-    </div>
-    </form> 
-               <button type="submit" className="btn " id="btn1">Submit</button>
 
-      
-              
+  const handlePopupOpen = () => {
+    setShowPopup(true);
+  };
+
+  const handlePopupClose = () => {
+    setShowPopup(false);
+  };
+
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="comment" onClick={handlePopupOpen}>
+          Your Comments
+        </div>
+      </div>
+
+      {showPopup && (
+        <div className="popup">
+          <div className="popup-content">
+            <button className="close-btn" onClick={handlePopupClose}>
+              Close
+            </button>
+            <div className="row">
+               <div className="col-md-2">  </div>
+              <div className="col-md-8" >
+            <form onSubmit={handleSubmit}>
+              <div className="form-group" style={{ height: "500px" }}>
+                <label htmlFor="exampleFormControlTextarea1" id="label1">
+                  We are eager to get your comments
+                </label>
+                <textarea
+                  className="form-control"
+                  id="exampleFormControlTextarea1"
+                  style={{ height: "500px" }}
+                  onChange={(e) => setSuggest(e.target.value)}
+                ></textarea>
+              </div>
+              <button type="submit" className="btn" id="btn1">
+                Submit
+              </button>
+            </form>
             </div>
-      </div> 
-</div>
-    )
+            <div className="col-md-2">  </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
-export default Car;
+
+export default Suggestion;
